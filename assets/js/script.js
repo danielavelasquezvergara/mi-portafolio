ca30e607e151bb2f70a2a2e9c8a1b348f9ec40b7
Daniela <dani.velasquezv@gmail.com>
2020-05-20 21:16:10 -0400

    modificacion js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,15 @@
-$(funtion(){
-    $("a").click(funtion(event){
-        if(this.hash !==""){
+$(function(){
+    $("a").click(function(event) {
+        if (this.hash !== "") {
             event.preventDefault();
-
             var gato = this.hash;
-            $("html, body").animate({
+            $('html, body').animate({
                 scrollTop: $(gato).offset().top
-            },800, funtion(){
-                window.location.hash = gato
-            });
-        }
-    });
+                }, 800, function(){
+                    window.location.hash = gato;
+        });
+            } 
+        });
     $('[data-togle="popover"]').popover();
 
     $('[data-toggle="tooltip"]').tooltip()
